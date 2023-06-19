b3ea9e312988348fb19c0afe567fcf32605ff7d0
titzko <matitzko@gmail.com>
2023-06-19 20:15:29 +0200

    more setting thimngs up and so on

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 import BootstrapTest from  './components/BootstrapTest';
-
+import { useEffect } from 'react';
 
 
 function App() {
+
+
+  // https://titzko.github.io/public_api/data/product_data.json
+  useEffect(()=> {
+    console.log("xd")
+
+      async function fetchProducts() {
+        const response = await fetch("https://titzko.github.io/public_api/data/product_data.json")
+        const products = await response.json();
+        console.log({products})
+      }
+      
+
+      fetchProducts();
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
