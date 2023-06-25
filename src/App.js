7efd3a14801eb910cc908b49ca4c6f7015d11f96
titzko <matitzko@gmail.com>
2023-06-25 16:31:36 +0200

    Pagination works

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import './App.scss';
-import { useEffect } from 'react';
 import Store from './components/Store';
 
 
 function App() {
 
-  const PRODUCT_DATA_URL = 'https://titzko.github.io/public_api/data/product_data.json';
-
-  useEffect(()=> {
-    
-
-      async function fetchProducts() {
-        const response = await fetch(PRODUCT_DATA_URL)
-        const products = await response.json();
-        console.log({products})
-      }
-      
-
-      fetchProducts();
-  })
 
 
   return (
-    <Store />
+    <Store /> //
   );
 }
 
--- src/components/Products/Pagination/Pagination.js
+++ src/components/Products/Pagination/Pagination.js
@@ -5,19 +5,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Pagination.scss';
 
 
-export default function Pagination() {
- 
-    
-    return(
+export default function Pagination({ totalPages, currentPage, onPageChange }) {
+
+
+
+    const handleNewPageClick = (newPage) => {
+        onPageChange(newPage)
+    }
+
+
+    const handlePreviousClick = () => {
+        (currentPage > 1) && onPageChange(currentPage - 1)
+    }
+
+
+    const handleNextClick = () => {
+        (currentPage < totalPages) && onPageChange(currentPage + 1)
+    }
+
+
+    return (
         <>
-        
-        <div className='pagination-wrapper'>
-                <div className='nav-btn'>
-                <FontAwesomeIcon icon={faChevronLeft} /><span className='no-display-mobile'> Previous</span> </div>
-                <div className='nav-btn'>1</div>
-                <div className='nav-btn'>2</div>
-                <div className='nav-btn'>3</div>
-                <div className='nav-btn'> <span className='no-display-mobile'>Next </span><FontAwesomeIcon icon={faChevronRight} /></div>
+            <div className='pagination-wrapper'>
+                <div className='nav-btn' onClick={() => { handlePreviousClick() }}>
+                    <FontAwesomeIcon icon={faChevronLeft} /><span className='no-display-mobile'> Previous</span>
+                </div>
+                
+                {Array.from(Array(totalPages).keys()).map((element, index) => {
+                    return (
+                        <div className={'nav-btn no-display-mobile' + ((currentPage == element + 1) ? ' current-page-btn' : '')}
+                            onClick={() => { handleNewPageClick(element + 1) }} key={index} >
+                            {element + 1}
+                        </div>
+                    )
+                })}
+                <div className='nav-btn' onClick={() => { handleNextClick() }}>
+                    <span className='no-display-mobile'>Next </span><FontAwesomeIcon icon={faChevronRight} />
+                </div>
             </div>
         </>
     )
